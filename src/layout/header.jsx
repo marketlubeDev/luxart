457856b41614e8602da4d2edb4e09707e777e86b
marketlube <marketlubedevelopers@gmail.com>
2025-05-28 17:51:52 +0530

    eighth

--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import logo from "../../src/assets/luxartlogo.svg";
-import phoneIcon from "../../src/assets/phone.svg";
+import phoneIcon from "../../src/assets/Phone.svg";
 import { Link } from "react-router-dom";
 
 const header = () => {
