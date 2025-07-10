90097629b61e92297a16a0fb48f3d8d7d0d0aa00
marketlube <marketlubedevelopers@gmail.com>
2025-07-10 12:33:47 +0530

    service section

--- src/page/hero/section/Quote.jsx
+++ src/page/hero/section/Quote.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Quote = () => {
   return (
     <section className="quote">
-      <p className="quote__text" style={{ width: "70%" }}>
+      <p className="quote__text">
         <span>Before you build up</span>, let’s sit down — every great space
         starts with a <strong>conversation.</strong>
       </p>
